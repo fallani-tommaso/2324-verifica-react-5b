12c21ca0f5048c23124efbec6243454acbb5ffb6
lai2-24 <lai2-24@itismeucci.com>
2024-05-06 10:43:42 +0200

    secondo commit

--- app/src/App.js
+++ app/src/App.js
@@ -6,16 +6,16 @@ function App() {
   const [partita, setPartita] = useState([]);
   const [inCaricamento, setInCaricamento] = useState(false);
   const [numero, setNumero] = useState();
-  const [showResult, setshowResult] = useState(false);
-  const [risultato, setRisultato] = useState([]);
-
+  const [r, setRisultato] = useState([]);
   function gestisciNumero(e) {
     setNumero(e.target.value);
   }
 
+  
+
   async function start(){
     setInCaricamento(true);
-    const response = await fetch("http://localhost:8080/partita", 
+    const response = await fetch(`http://localhost:8080/partita`, 
       {method: "POST",
       headers: { "Content-type": "application/json" },
     });
@@ -23,11 +23,10 @@ function App() {
     const partita_get = await response.json();
     setPartita(partita_get);
     setInCaricamento(false);
-    setshowResult(true)
   }
 
   async function invia(){
-    const response = await fetch("http://localhost:8080/partita", 
+    const response = await fetch(`http://localhost:8080/partita/${partita.id}`, 
       {method: "PUT",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({ numero : numero })
@@ -35,34 +34,41 @@ function App() {
 
     const partita_get = await response.json();
     setRisultato(partita_get);
-    setshowResult(true)
   }
-
-  /*function MostraRisultato(){
-    if(risultato> partita.)
-  }*/
 
   return (
     <div className="App">
       <p>Indovina Numero</p>
       <button onClick={start} className="bottone">Nuova partita</button>
-      { 
+      {
         inCaricamento ?
           <div className="carica">In caricamento...</div>
         :
         (
-          <>
+          <div>
           <p>ID: {partita.id}<br>
-          </br>Tentativi: {partita.tentativi}</p>
+          </br>Tentativi: {r.tentativi}</p>
           <p>Inserisci un numero tra 1 e 100:  <br>
           </br>
           <input type="number" onChange={gestisciNumero} value={numero} placeholder=""/>
           <button onClick={invia} className="bottone">Invia</button></p>
-          
-          </>
+          </div>
         )
       }
-    </div>
+      {
+        r.risultato === 0 &&
+        <p>COMPLIMENTI HAI INDOVINATO IN {r.tentativi} TENTATIVI</p>
+      }
+      {
+        r.risultato === 1 &&
+        <p>TROPPO GRANDE</p>
+      }
+      {
+        r.risultato === -1 &&
+        <p>TROPPO PICCOLO</p>
+      }
+
+      </div>
   );
 }
 
